497fb8aa1209e459c2a6964c7f99a20e4c56f253
lucassalves <lucassalves12@gmail.com>
2021-11-25 22:25:46 -0300

    Add dependency injection

---
+++ profits-distribution/ProfitsDistribution.CC.IoC/DependencyInjection/ConfigureService.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace ProfitsDistribution.CC.IoC.DependencyInjection
+{
+    public class ConfigureService
+    {
+        public static void RegisterServicesDependencies(IServiceCollection services)
+        {
+
+        }
+    }
+}
